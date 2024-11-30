b7e9f088cb2365a09a4d6277efb0a771c60fdeab
Yarkovyy <Timofey.yarkovoy@gmail.com>
2024-11-30 19:37:01 +0200

    User

---
+++ StorageWPF/Model/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageWPF.Model
+{
+    internal class User
+    {
+        private string username;
+        private string password;
+
+        public string Username { get { return username; } }
+        public string Password { get { return password; } }        
+    }
+}
