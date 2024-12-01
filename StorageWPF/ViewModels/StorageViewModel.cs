9ef79cbc9aeb5205b8dbbd378dfc251ce5604340
Nomad232 <zanrjnbr@gmail.com>
2024-12-01 20:04:50 +0200

    Добавление комманд для верхней панели

--- StorageWPF/ViewModels/StorageViewModel.cs
+++ StorageWPF/ViewModels/StorageViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using StorageWPF.Models;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Windows.Input;
+using System.Windows;
 using System.Xml.Serialization;
 
 namespace StorageWPF.ViewModels
@@ -17,6 +19,55 @@ namespace StorageWPF.ViewModels
             {
                 XmlSerializer xmlSerializer = new XmlSerializer(typeof(ObservableCollection<Product>));
                 Products = (ObservableCollection<Product>)xmlSerializer.Deserialize(f);
+            }
+        }
+
+        private RelayCommand closeCommand;
+        public RelayCommand CloseCommand
+        {
+            get
+            {
+                return closeCommand ??
+                    (closeCommand = new RelayCommand(obj =>
+                    {
+                        if (obj is Window window)
+                        {
+                            window.Close();
+                        }
+                    }));
+            }
+        }
+        private RelayCommand minimizeCommand;
+        public RelayCommand MinimizeCommand
+        {
+            get
+            {
+                return minimizeCommand ??
+                    (minimizeCommand = new RelayCommand(obj =>
+                    {
+                        if (obj is Window window)
+                        {
+                            window.WindowState = WindowState.Minimized;
+                        }
+                    }));
+            }
+        }
+        private RelayCommand dragCommand;
+        public RelayCommand DragCommand
+        {
+            get
+            {
+                return dragCommand ??
+                    (dragCommand = new RelayCommand(obj =>
+                    {
+                        if (obj is Window window)
+                        {
+                            if (Mouse.LeftButton == MouseButtonState.Pressed)
+                            {
+                                window.DragMove();
+                            }
+                        }
+                    }));
             }
         }
 
