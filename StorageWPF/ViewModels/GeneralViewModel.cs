2d26147d3c234bec50720041ee8853d0d106a6d2
Yarkovyy <Timofey.yarkovoy@gmail.com>
2024-12-02 21:23:29 +0200

    GeneralViewModel base

---
+++ StorageWPF/ViewModels/GeneralViewModel.cs
@@ -0,0 +1,19 @@
+﻿using StorageWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageWPF.ViewModels
+{
+    internal class GeneralViewModel: ViewModel
+    {
+        public ObservableCollection<Product> Products { get; }
+        public GeneralViewModel(ObservableCollection<Product> Products)
+        {
+            this.Products = Products;
+        }
+    }
+}
