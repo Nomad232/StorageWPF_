07c5a83fe5f11d735b56d2c0e9f01f783c313e9c
Nomad232 <zanrjnbr@gmail.com>
2024-12-04 15:45:39 +0200

    Добавление графика на окно

--- StorageWPF/ViewModels/GeneralViewModel.cs
+++ StorageWPF/ViewModels/GeneralViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using OxyPlot;
+using OxyPlot.Axes;
 using OxyPlot.Series;
 using StorageWPF.Models;
 using System;
@@ -11,10 +12,10 @@ using System.Xml.Linq;
 
 namespace StorageWPF.ViewModels
 {
-    internal class GeneralViewModel: ViewModel
+    internal class GeneralViewModel : ViewModel
     {
-        public string[] Labels => Products?.Select(e => e.Name).ToArray();
-        public double[] Values => Products?.Select(e => (double)e.Count).ToArray();
+        public string[] Labels => Products?.OrderBy(x=> x.Count).Select(e => e.Name).ToArray();
+        public double[] Values => Products?.OrderBy(x => x.Count).Select(e => (double)e.Count).ToArray();
         public ObservableCollection<Product> Products { get; set; }
         public GeneralViewModel(ObservableCollection<Product> products)
         {
@@ -22,8 +23,9 @@ namespace StorageWPF.ViewModels
             OnPropertyChanged("Lables");
             OnPropertyChanged("Values");
 
-            PlotModel = new PlotModel { Title = "Динамический график" };
-            UpdatePlot();
+            PlotModel = new PlotModel { Title = "Quantity graph" };
+
+            StartAsyncUpdate();
         }
 
         private PlotModel _plotModel;
@@ -38,29 +40,51 @@ namespace StorageWPF.ViewModels
             }
         }
 
+        private async Task StartAsyncUpdate()
+        {
+            while (true)
+            {
+                UpdatePlot();
+                await Task.Delay(5000);
+            }
+        }
+
         private void UpdatePlot()
         {
-            // Обновляем график
+            // Очистка предыдущего графика
             PlotModel.Series.Clear();
+            PlotModel.Axes.Clear();
 
+            // Создание серии данных
             var barSeries = new BarSeries
             {
-                ItemsSource = Values,
+                ItemsSource = Values.Select(value => new BarItem { Value = value }).ToList(),
                 LabelPlacement = LabelPlacement.Inside,
                 LabelFormatString = "{0}"
             };
 
+            // Добавление серии на график
             PlotModel.Series.Add(barSeries);
 
-            PlotModel.Axes.Clear();
-            PlotModel.Axes.Add(new OxyPlot.Axes.CategoryAxis
+            // Настройка оси Y (категорийная ось с метками)
+            PlotModel.Axes.Add(new CategoryAxis
             {
-                Position = OxyPlot.Axes.AxisPosition.Left,
+                Position = AxisPosition.Left,
                 ItemsSource = Labels
             });
 
-            // Сообщаем, что график обновился
+            // Настройка оси X (числовая ось для значений)
+            PlotModel.Axes.Add(new LinearAxis
+            {
+                Position = AxisPosition.Bottom,
+                MinimumPadding = 0.1, // Дополнительное пространство слева
+                MaximumPadding = 0.1, // Дополнительное пространство справа
+                Title = "Quantity"      // Подпись оси X
+            });
+
+            // Обновление графика
             PlotModel.InvalidatePlot(true);
         }
+
     }
 }
